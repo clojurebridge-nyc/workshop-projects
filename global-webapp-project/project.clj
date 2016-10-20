b86597e7266fb2da3b11dfe92b2126557ad43397
Nola Stowe <nola@rubygeek.com>
2016-10-19 21:32:35 -0500

    updated

---
+++ global-webapp-project/project.clj
@@ -0,0 +1,17 @@
+(defproject global-growth "0.1.0-SNAPSHOT"
+  :description "Demonstrates the use of the World Bank API"
+  :url "https://github.com/clojurebridge/global-growth"
+  :license {:name "Creative Commons Attribution License"
+            :url "http://creativecommons.org/licenses/by/3.0/"}
+  :plugins [[lein-ring "0.8.10"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.7.7"]
+                 [cheshire "5.2.0"]
+                 [ring "1.2.1"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.4"]]
+  :min-lein-version "2.0.0"
+  :main ^:skip-aot global-growth.core
+  :ring {:handler global-growth.web/handler}
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
